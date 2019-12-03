499059c2eb5c14d6729517f47b2462d8a5dfeeee
Andrew who don't know programming <agbazarkin@edu.hse.ru>
2019-12-03 15:42:10 +0300

    version 3.1

--- src/task3.cpp
+++ src/task3.cpp
@@ -7,6 +7,6 @@ unsigned long long sumPrime(unsigned int hbound){
 	unsigned long long sum = 0;
 	for (int i = 2; i < hbound; i++)
 		if(checkPrime(i))
-			sum += i
-	return sum
+			sum += i;
+	return sum;
 }
