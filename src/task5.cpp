75edbb476e40b3e99541a209cbf3cefa16c20ee2
Andrew who don't know programming <agbazarkin@edu.hse.ru>
2019-12-17 22:28:47 +0300

    version 6.4

--- src/task5.cpp
+++ src/task5.cpp
@@ -8,14 +8,14 @@ void split(char ***result, int *N, char *buf, char ch){
 		if(*(buf + i) == ch)
 			*N += 1;
 	*result = (char**)malloc(*N * sizeof(char*));
-	i = 0;
+	int i = 0;
 	for(int j = 0; j < N*; j++){
 		int tmplen = 0;
 		while((*(buf + 1) != 0) && (*(buf + 1) != ch)){
 			i++;
 			tmplen++;
 		}
-		*(result + j) = (char*)malloc((lenstr + 1) * sizeof(char));
+		*(result + j) = (char*)malloc((tmplen + 1) * sizeof(char));
 		strncpy(*(*result + j), buf + i - tmplen, tmplen);
 		*(*(*result + j) + tmplen) = 0;
 		i++;
